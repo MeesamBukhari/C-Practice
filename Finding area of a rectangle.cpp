90e3022f9cc26f031a7271c592c2e128acec1d71
Syed Meesam Turab <meesamturab@gmail.com>
2023-06-27 07:08:10 -0700

    Practice - C++

---
+++ Finding area of a rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namsespace std;
+int main()
+{
+    int length, breadth, area;
+    cout << "Enter length of rectangle: ";
+    cin >> length;
+    cout << "Enter breadth of rectangle: ";
+    cin >> breadth;
+    cout << "The area of rectangle is " << area = length * breadth;
+    return 0;
+}
---
+++ Finding sum of two numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    c = a + b;
+    cout << "The sum of both numbers is " << c << endl;
+    return 0;
+}
